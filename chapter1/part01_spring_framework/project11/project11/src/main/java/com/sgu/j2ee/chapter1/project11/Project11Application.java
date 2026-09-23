fe5cdcce2c093d990334e365a0c2c53680e5c147
HuynhThaiKiet <htk2k6@gmail.com>
2026-09-23 17:46:52 +0700

    add new maven project from Spring Initializer

---
+++ chapter1/part01_spring_framework/project11/project11/src/main/java/com/sgu/j2ee/chapter1/project11/Project11Application.java
@@ -0,0 +1,13 @@
+package com.sgu.j2ee.chapter1.project11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project11Application.class, args);
+	}
+
+}
---
+++ chapter1/part01_spring_framework/project11/project11/src/test/java/com/sgu/j2ee/chapter1/project11/Project11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgu.j2ee.chapter1.project11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
